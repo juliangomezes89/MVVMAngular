66cefdf57e7aa96967dbd2a71c849fae1e32b310
juliangoes89 <juliangomezes89@gmail.com>
2020-03-14 15:15:17 -0500

    Estilos y comunicacion entre componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-app';
+  contador: number;
+  constructor(){
+    this.contador = 10;
+  }
+  incrementar(){
+    this.contador += 1;
+  }
+  decrementar(){
+    this.contador -= 1;
+  }
 }
