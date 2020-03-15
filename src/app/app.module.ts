2f97353f8ba05fb67d881fc2cfc007d7a34efba0
juliangoes89 <juliangomezes89@gmail.com>
2020-03-15 05:39:28 -0500

    Estructurar las carpetas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HijoComponent } from './contador/hijo/hijo.component';
-import { NietoComponent } from './contador/nieto/nieto.component';
+import { NietoComponent } from './contador/hijo/nieto/nieto.component';
 
 @NgModule({
   declarations: [
